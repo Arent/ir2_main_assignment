a061546e14fe87184e2dbc6cdfe6be604a24388a
Bryan Eikema <bryan.eikema@student.uva.nl>
2017-11-20 17:24:27 +0100

    Merge in part of Arent's code; make everything controllable through the cmd line

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ import utils
 
 from qarnn import QARNN
 
+OPTIMIZER_DICT = {
+                  "sgd": tf.train.GradientDescentOptimizer, # Gradient Descent
+                  "adadelta": tf.train.AdagradDAOptimizer, # Adadelta
+                  "adagrad": tf.train.AdagradOptimizer, # Adagrad
+                  "adam": tf.train.AdamOptimizer, # Adam
+                  "rmsprop": tf.train.RMSPropOptimizer # RMSprop
+                  }
+
 def qualitative_analysis(contexts, questions, answers, context_lengths,
                          question_lengths, predictions, i2w, k=1):
   size = questions.shape[0]
@@ -30,6 +38,15 @@ def qualitative_analysis(contexts, questions, answers, context_lengths,
     print("Model prediction: %s" % prediction_)
     print("-------------------------")
 
+def parse_cell_type(cell_type):
+  if cell_type == "gru":
+    return tf.contrib.rnn.GRUCell
+  elif cell_type == "lstm":
+    return tf.contrib.rnn.LSTMCell
+  else:
+    print("ERROR: unknown cell type")
+    return None
+
 # Parse cmd line args.
 parser = argparse.ArgumentParser()
 parser.add_argument("--data_dir", type=str, default=None,
@@ -48,11 +65,39 @@ parser.add_argument("--max_gradient_norm", type=float, default=1.0,
                     help="Maximum norm for gradients.")
 parser.add_argument("--model_dir", type=str, default=None,
                     help="Directory to store the model parameters.")
+parser.add_argument("--cell_type", type=str, default="lstm",
+                    help="Cell type for the RNN.")
+parser.add_argument("--num_units", type=int, default=64,
+                    help="Number of hidden units in the RNN encoder")
+parser.add_argument("--embedding_size", type=int, default=64,
+                    help="Size of the word embeddings.")
+parser.add_argument("--encoder_type", type=str, default="uni",
+                    help="uni|bi")
+parser.add_argument("--dropout_keep_prob", type=float, default=0.7,
+                    help="Dropout keep probability")
+parser.add_argument("--num_output_hidden", type=str, default="256",
+                    help="Number of units or the hidden layers, formatted as h1_size,h2_size,etc")
+parser.add_argument("--merge_mode", type=str, default="concat",
+                    help="sum|concat")
+parser.add_argument("--num_enc_layers", type=int, default=1,
+                    help="Number of encoder layers in the encoder")
+parser.add_argument("--optimizer", type=str, default="adam",
+                    help="sgd|adam|adagrad|rmsprop")
 args = parser.parse_args()
 
 if args.data_dir is None or args.model_dir is None:
   print("--data_dir and/or --model_dir argument missing.")
   sys.exit(1)
+
+optimizer = OPTIMIZER_DICT[args.optimizer]
+
+if args.num_output_hidden:
+  num_output_hidden = args.num_output_hidden.split(",")
+  num_output_hidden = [int(num_hidden) for num_hidden in num_output_hidden]
+else:
+  num_output_hidden = []
+
+cell_type = parse_cell_type(args.cell_type)
 
 # Load the vocabulary.
 w2i, i2w = utils.load_vocab(args.vocab)
@@ -62,34 +107,57 @@ print("Vocabulary size: %d" % vocab_size)
 
 # Load the training / testing data.
 train, test = utils.load_data(args.task, args.data_dir, args.vocab, tokenizer, args.batch_size)
-contexts, questions, answers, context_lengths, question_lengths = train.get_next()
-test_contexts, test_questions, test_answers, test_context_lengths, test_question_lengths = test.get_next()
+context, question, answer, context_length, question_length = train.get_next()
+test_context, test_question, test_answer, test_context_length, test_question_length = test.get_next()
 
 # Create the training model.
 with tf.variable_scope("root"):
-  train_model = QARNN(tf.contrib.learn.ModeKeys.TRAIN, vocab_size, contexts,
-      questions, answers, context_lengths, question_lengths)
-  train_loss = train_model.loss()
-  optimizer = tf.train.AdamOptimizer(args.learning_rate)
+  train_model = QARNN(tf.contrib.learn.ModeKeys.TRAIN, vocab_size,
+      embedding_size=args.embedding_size, num_units=args.num_units,
+      encoder_type=args.encoder_type, keep_prob=args.dropout_keep_prob,
+      cell_type=cell_type, num_output_hidden=num_output_hidden,
+      num_enc_layers=args.num_enc_layers, merge_mode=args.merge_mode,
+      optimizer=optimizer, learning_rate=args.learning_rate,
+      max_gradient_norm=args.max_gradient_norm)
 
-  # Clip the gradients before applying them.
-  params = tf.trainable_variables()
-  gradients = tf.gradients(train_loss, params)
-  clipped_gradients, _ = tf.clip_by_global_norm(gradients,
-      args.max_gradient_norm)
-  train_op = optimizer.apply_gradients(
-      zip(clipped_gradients, params))
-
+  # Build the training model graph.
+  encoded_context = train_model.create_encoder("context_encoder",
+      context, context_length)
+  encoded_question = train_model.create_encoder("question_encoder",
+      question, question_length)
+  merged_encoding = train_model.merge_encodings(encoded_context,
+      encoded_question)
+  logits = train_model.create_decoder(merged_encoding)
+  train_loss = train_model.loss(logits, answer)
+  train_acc = train_model.accuracy(logits, answer)
+  train_op = train_model.train_step(train_loss)
 
 # Create the testing model.
 with tf.variable_scope("root", reuse=True):
-  test_model = QARNN(tf.contrib.learn.ModeKeys.EVAL, vocab_size, test_contexts,
-      test_questions, test_answers, test_context_lengths, test_question_lengths)
-  test_acc = test_model.accuracy()
+  test_model = QARNN(tf.contrib.learn.ModeKeys.EVAL, vocab_size,
+      embedding_size=args.embedding_size, num_units=args.num_units,
+      encoder_type=args.encoder_type, keep_prob=args.dropout_keep_prob,
+      cell_type=cell_type, num_output_hidden=num_output_hidden,
+      num_enc_layers=args.num_enc_layers, merge_mode=args.merge_mode,
+      optimizer=optimizer, learning_rate=args.learning_rate,
+      max_gradient_norm=args.max_gradient_norm)
+
+  # Build the testing model graph.
+  test_encoded_context = test_model.create_encoder("context_encoder",
+      test_context, test_context_length)
+  test_encoded_question = test_model.create_encoder("question_encoder",
+      test_question, test_question_length)
+  test_merged_encoding = test_model.merge_encodings(test_encoded_context,
+      test_encoded_question)
+  test_logits = test_model.create_decoder(test_merged_encoding)
+  test_acc = test_model.accuracy(test_logits, test_answer)
 
 # Create Tensorboard summaries.
-loss_summary = tf.summary.scalar("train_loss", train_loss)
-acc_summary = tf.summary.scalar("test_accuracy", test_acc)
+train_loss_summary = tf.summary.scalar("train_loss", train_loss)
+train_acc_summary = tf.summary.scalar("train_acc", train_acc)
+train_summaries = tf.summary.merge([train_loss_summary, train_acc_summary])
+
+test_summaries = tf.summary.scalar("test_accuracy", test_acc)
 
 # Parameter saver.
 saver = tf.train.Saver()
@@ -115,9 +183,8 @@ with tf.Session() as sess:
   # Evaluate before training.
   sess.run(test.initializer)
   contexts, questions, answers, context_lengths, question_lengths, predictions, acc, summary = sess.run([
-      test_model.context, test_model.question, test_model.answer,
-      test_model.context_length, test_model.question_length,
-      tf.argmax(test_model.logits, axis=-1), test_acc, acc_summary])
+      test_context, test_question, test_answer, test_context_length, test_question_length,
+      tf.argmax(test_logits, axis=-1), test_acc, test_summaries])
   print("---- Qualitative Analysis")
   qualitative_analysis(contexts, questions, answers, context_lengths, question_lengths, predictions, i2w, k=3)
   print("Test accuracy: %f" % acc)
@@ -128,7 +195,7 @@ with tf.Session() as sess:
 
     try:
       # Train on all batches for one epoch.
-      _, loss, summary = sess.run([train_op, train_loss, loss_summary])
+      _, loss, summary = sess.run([train_op, train_loss, train_summaries])
       step += 1
       total_step += 1
       train_writer.add_summary(summary, total_step)
@@ -147,9 +214,8 @@ with tf.Session() as sess:
       # Evaluate the model on the test set.
       sess.run(test.initializer)
       contexts, questions, answers, context_lengths, question_lengths, predictions, acc, summary = sess.run([
-          test_model.context, test_model.question, test_model.answer,
-          test_model.context_length, test_model.question_length,
-          tf.argmax(test_model.logits, axis=-1), test_acc, acc_summary])
+          test_context, test_question, test_answer, test_context_length, test_question_length,
+          tf.argmax(test_logits, axis=-1), test_acc, test_summaries])
       print("---- Qualitative Analysis")
       qualitative_analysis(contexts, questions, answers, context_lengths, question_lengths, predictions, i2w, k=3)
       print("Test accuracy: %f" % acc)
--- qarnn.py
+++ qarnn.py
@@ -3,126 +3,143 @@ import numpy as np
 
 class QARNN:
 
-  def __init__(self, mode, vocab_size, contexts, questions, answers, context_lengths, question_lengths,
-      embedding_size=64, num_units=64, activation=tf.nn.relu, encoder_type="bi", keep_prob=0.7,
-      cell_type="gru", num_output_hidden=[256]):
+  def __init__(self, mode, vocab_size, embedding_size=64, num_units=64, encoder_type="bi", keep_prob=0.7,
+      cell_type=tf.contrib.rnn.BasicLSTMCell, num_output_hidden=[256], num_enc_layers=1, merge_mode="concat",
+      optimizer=tf.train.AdamOptimizer, learning_rate=0.001, max_gradient_norm=1.0):
     self.embedding_size = embedding_size
     self.num_units = num_units
-    self.activation = activation
     self.vocab_size = vocab_size
     self.encoder_type = encoder_type
     self.keep_prob = keep_prob
     self.cell_type = cell_type
     self.num_output_hidden = num_output_hidden
+    self.num_enc_layers = num_enc_layers
+    self.merge_mode = merge_mode
+    self.optimizer = optimizer
+    self.learning_rate = learning_rate
+    self.max_gradient_norm = max_gradient_norm
     self.mode = mode
 
-    self.context = contexts
-    self.question = questions
-    self.answer = answers
-    self.context_length = context_lengths
-    self.question_length = question_lengths
+  # Creates an encoder on the given sequence, returns the final state of the encoder.
+  def create_encoder(self, name, sequence, sequence_length):
 
-    self._create_model()
+    with tf.variable_scope(name) as scope:
+      embedding_matrix = tf.get_variable("embedding_matrix", \
+          [self.vocab_size, self.embedding_size], dtype=tf.float32)
+      embeddings = tf.nn.embedding_lookup(embedding_matrix,
+          sequence) # [batch, time, emb_size]
 
-  def _create_model(self):
-
-    # mode = "c_plus_q" # c_plus_q|shared|separate
-    # emb_mode = "shared" # shared|separate
-    # c + q
-    # c_and_q = tf.concat([self.context, self.question], axis=-1)
-    # joint_embeddings = tf.nn.embedding_lookup(embedding_matrix, c_and_q)
-
-    # retrieve embeddings
-    context_embedding_matrix = tf.get_variable("context_embedding_matrix", \
-        [self.vocab_size, self.embedding_size], dtype=tf.float32)
-    question_embedding_matrix = tf.get_variable("question_embedding_matrix", \
-        [self.vocab_size, self.embedding_size], dtype=tf.float32)
-    context_embeddings = tf.nn.embedding_lookup(context_embedding_matrix,
-        self.context)
-    question_embeddings = tf.nn.embedding_lookup(question_embedding_matrix,
-        self.question) # [1, time, emb_size]
-
-    # Perform dropout on the embeddings. (Is this common?)
-    if self.mode == tf.contrib.learn.ModeKeys.TRAIN:
-      context_embeddings = tf.nn.dropout(context_embeddings, self.keep_prob)
-      question_embeddings = tf.nn.dropout(question_embeddings, self.keep_prob)
-
-    # Run the LSTM on the context.
-    outputs, final_state = self._create_encoder(context_embeddings, self.context_length,
-        name="context_encoder", reuse=False)
-    encoded_context = final_state
-
-    # Run the LSTM on the question.
-    outputs, final_state = self._create_encoder(question_embeddings, self.question_length,
-        name="question_encoder", reuse=False)
-    encoded_question = final_state
-
-    # Combine the story and question.
-    encoder_output = tf.concat([encoded_context, encoded_question], axis=-1)
-    # encoder_output = encoded_context + encoded_question
-
-    # Perform dropout when training.
-    prev_h = encoder_output
-    for num_hidden_units in self.num_output_hidden:
-      h = tf.layers.dense(prev_h, num_hidden_units,
-          activation=self.activation, use_bias=False)
-      if self.mode == tf.contrib.learn.ModeKeys.TRAIN:
-        h = tf.nn.dropout(h, self.keep_prob)
-      prev_h = h
-
-    # Predict the answer word.
-    self.logits = tf.layers.dense(h, self.vocab_size,
-        activation=None, use_bias=False)
-
-  def _create_cell(self):
-    if self.cell_type == "gru":
-      cell = tf.contrib.rnn.GRUCell(self.num_units,
-          activation=self.activation)
-    elif self.cell_type == "lstm":
-      cell = tf.contrib.rnn.LSTMCell(self.num_units,
-          activation=self.activation)
-    else:
-      print("ERROR: unknown cell type")
-      cell = None
-    return cell
-
-  def _create_encoder(self, inputs, sequence_length, name="encoder", reuse=False):
-    with tf.variable_scope(name, reuse=reuse) as scope:
       if self.encoder_type == "uni":
-        cell = self._create_cell()
-        outputs, final_state = tf.nn.dynamic_rnn(cell, inputs,
+
+        # Build a multilayer RNN encoder.
+        cells = []
+        for _ in range(self.num_enc_layers):
+          cell = self.cell_type(self.num_units)
+
+          # Perform dropout during training.
+          if self.mode == tf.contrib.learn.ModeKeys.TRAIN:
+            cell = tf.contrib.rnn.DropoutWrapper(cell, input_keep_prob=self.keep_prob)
+
+          cells.append(cell)
+
+        cell = tf.contrib.rnn.MultiRNNCell(cells)
+
+        _, final_state = tf.nn.dynamic_rnn(cell, embeddings,
             dtype=tf.float32, sequence_length=sequence_length)
 
-        if self.cell_type == "lstm":
+        # For multilayer RNNs, use the final layer's state.
+        final_state = final_state[-1]
+
+        # For tuple states such as LSTM, return only h as the encoded
+        if isinstance(final_state, tuple):
           final_state = final_state.h
+
+        return final_state
       else:
-        fw_cell = self._create_cell()
-        bw_cell = self._create_cell()
+
+        # Bidirectional encoders can have only 1 layer in our implementation.
+        assert self.num_enc_layers == 1
+
+        fw_cell = self.cell_type(self.num_units)
+        bw_cell = self.cell_type(self.num_units)
+
+        # Perform dropout during training.
+        if self.mode == tf.contrib.learn.ModeKeys.TRAIN:
+          fw_cell = tf.contrib.rnn.DropoutWrapper(fw_cell, output_keep_prob=self.keep_prob)
+          bw_cell = tf.contrib.rnn.DropoutWrapper(bw_cell, output_keep_prob=self.keep_prob)
 
         # Run the bidirectional RNN.
-        bi_outputs, bi_final_state = tf.nn.bidirectional_dynamic_rnn(
+        _, bi_final_state = tf.nn.bidirectional_dynamic_rnn(
             fw_cell,
             bw_cell,
-            inputs,
+            embeddings,
             sequence_length=sequence_length,
             dtype=tf.float32)
 
-        # Concatenate the forward and backward layer to create the encoder output.
-        outputs = tf.concat(bi_outputs, axis=-1)
-
-        if self.cell_type == "lstm":
+        # For tuple states such as LSTM, return only h as the encoded, concatenate
+        # forward and backward RNN outputs.
+        if isinstance(final_state, tuple):
           final_state = tf.concat([bi_final_state[1].h, bi_final_state[1].h], axis=-1)
         else:
           final_state = tf.concat(bi_final_state, axis=-1)
-    return outputs, final_state
 
-  def loss(self):
+    return final_state
+
+  # Combine the context and question encoding.
+  def merge_encodings(self, encoded_context, encoded_question):
+    if self.merge_mode == "sum":
+      merged_encoding = encoded_context + encoded_question
+    elif self.merge_mode == "concat":
+      merged_encoding = tf.concat([encoded_context, encoded_question], axis=-1)
+    else:
+      print("ERROR: unknown merge mode")
+      merged_encoding = None
+    return merged_encoding
+
+  # Create an MLP decoder.
+  def create_decoder(self, decoder_inputs):
+    with tf.variable_scope("decoder"):
+
+      # Create the hidden layers.
+      prev_h = decoder_inputs
+      for num_hidden_units in self.num_output_hidden:
+        h = tf.layers.dense(prev_h, num_hidden_units,
+            activation=tf.nn.relu, use_bias=True)
+
+        # Perform dropout when training.
+        if self.mode == tf.contrib.learn.ModeKeys.TRAIN:
+          h = tf.nn.dropout(h, self.keep_prob)
+
+        prev_h = h
+
+      # Predict the answer word using a dense output layer to vocab size.
+      logits = tf.layers.dense(h, self.vocab_size,
+          activation=None, use_bias=False)
+
+    return logits
+
+  def loss(self, logits, answer):
     cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
-        labels=self.answer,
-        logits = self.logits,
+        labels=answer,
+        logits=logits,
         name="cross_entropy_loss")
     return tf.reduce_mean(cross_entropy)
 
-  def accuracy(self):
-    prediction = tf.argmax(self.logits, axis=-1, output_type=self.answer.dtype)
-    return tf.reduce_mean(tf.cast(tf.equal(self.answer, prediction), tf.float32))
+  def train_step(self, loss):
+    optimizer = self.optimizer(self.learning_rate)
+
+    # Clip the gradients before applying them.
+    params = tf.trainable_variables()
+    gradients = tf.gradients(loss, params)
+    clipped_gradients, _ = tf.clip_by_global_norm(gradients,
+        self.max_gradient_norm)
+
+    # Apply the gradients
+    train_op = optimizer.apply_gradients(
+        zip(clipped_gradients, params))
+
+    return train_op
+
+  def accuracy(self, logits, answer):
+    prediction = tf.argmax(logits, axis=-1, output_type=answer.dtype)
+    return tf.reduce_mean(tf.cast(tf.equal(answer, prediction), tf.float32))
