9dd724c18f3b75cb75b0fdf463c96f6362d8c493
Bram van den Akker <git@bramvandenakker.nl>
2017-11-22 16:08:07 +0100

    auto setup for the data

---
+++ data/build_vocab.py
@@ -0,0 +1,27 @@
+from nltk.tokenize.moses import MosesTokenizer
+from collections import defaultdict
+
+import operator
+import glob
+import os
+
+
+filenames = glob.glob("en/*.txt")
+
+tokenizer = MosesTokenizer()
+vocab = defaultdict(int)
+
+for filename in filenames:
+  with open(filename) as f:
+    for line in f:
+      tokens = tokenizer.tokenize(line.lower())
+      for token in tokens:
+        vocab[token] += 1
+
+words_by_freq = sorted(vocab.items(), key=operator.itemgetter(1))[::-1]
+with open("vocab.txt", "w+") as f:
+  for word, count in words_by_freq:
+    f.write(word)
+    f.write("\n")
+
+print("Wrote vocabulary to vocab.txt")
--- setup.sh
+++ setup.sh
@@ -10,12 +10,20 @@ rm tasks_1-20_v1-2.tar.gz
 mv tasks_1-20_v1-2/* ./
 rm -r tasks_1-20_v1-2
 
-cd ..
+# Build the vocab
+echo "[debug] - Building vocab"
+python3 build_vocab.py
+echo "[debug] - Vocab ready"
 
 # Create all seq2seq datasets
-
-mkdir data/seq2seq
+echo "[debug] - Creating seq2seq data"
+mkdir seq2seq
+cd ..
 for TASK in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
 	python3 2seq2seq.py --task=$TASK
 done
+echo "[debug] - Seq2seq data ready"
+
+
+
