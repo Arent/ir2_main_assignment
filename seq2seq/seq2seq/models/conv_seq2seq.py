db73259738b1ec106e4e50ea1fc2cebb4dd5c502
Bram van den Akker <git@bramvandenakker.nl>
2017-11-29 15:14:32 +0100

    smaller model

--- seq2seq/seq2seq/models/conv_seq2seq.py
+++ seq2seq/seq2seq/models/conv_seq2seq.py
@@ -70,10 +70,10 @@ class ConvSeq2Seq(Seq2SeqModel):
         "inference.beam_search.choose_successors_fn": "choose_top_k",
         "vocab_source": "",
         "vocab_target": "", 
-        "optimizer.name": "Momentum",
-        "optimizer.learning_rate": 0.25,
-        "optimizer.params": {"momentum": 0.99, "use_nesterov": True}, # Arbitrary parameters for the optimizer
-        #"optimizer.params": { "epsilon": 0.0000008}, # Arbitrary parameters for the optimizer
+        "optimizer.name": "Adam",
+        "optimizer.learning_rate": 0.01,
+        # "optimizer.params": {"momentum": 0.99, "use_nesterov": True}, # Arbitrary parameters for the optimizer
+        "optimizer.params": { "epsilon": 0.0000008}, # Arbitrary parameters for the optimizer
         "optimizer.lr_decay_type": "exponential_decay",
         "optimizer.lr_decay_steps": 5000,  # one epoch steps
         "optimizer.lr_decay_rate": 0.9,  
