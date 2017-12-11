64d0a101aa32ab44f0ffad514ef0e902dc5ef20c
Bram van den Akker <git@bramvandenakker.nl>
2017-12-11 11:38:58 +0100

    removed indents in gridsearch jobs

--- seq2seq/gridsearch/create_gridsearch.py
+++ seq2seq/gridsearch/create_gridsearch.py
@@ -20,50 +20,50 @@ for params in grid_params:
     file_name = "SEARCH_" + "-".join([key + "_" + str(search[key]) for key in id2key])
 
     header = """#!/bin/bash
-        #SBATCH -N 1
-        #SBATCH -p short
-        #SBATCH -t 00:30:00
-        #SBATCH -o {name}.output
-        #SBATCH -e {name}.error
+#SBATCH -N 1
+#SBATCH -p short
+#SBATCH -t 00:30:00
+#SBATCH -o {name}.output
+#SBATCH -e {name}.error
 
-        module load python/3.5.2 gcc/5.2.0 cuda/8.0.44 cudnn/8.0-v6.0
+module load python/3.5.2 gcc/5.2.0 cuda/8.0.44 cudnn/8.0-v6.0
 
-    export MODEL_DIR=./models/tasks/{name}
-    export DATA_PATH=../data""".format(name=file_name)
+export MODEL_DIR=./models/tasks/{name}
+export DATA_PATH=../data""".format(name=file_name)
 
     content = """
-    export DATA_PATH=../data
+export DATA_PATH=../data
 
-    export TRAIN_STEPS=10000
-    export MIN_EVAL=1000
-    export BATCH_SIZE=50
-    export LEARNING_RATE={learning_rate}
-    export SAVE_CHECKPOINTS_STEPS=$TRAIN_STEPS
-    export KEEP_RATE={keep_rate}
-    export EMBED_SIZE={embed_sizes}
-    export IN_LAYERS={num_layers}
-    export IN_HIDDEN={hidden}
-    export IN_KERNEL_SIZE={hidden}
-    export OUT_LAYERS=2
-    export OUT_HIDDEN=16,16
-    export OUT_KERNEL_SIZE=4,4
-    export MAX_LENGHT=1200 #1891
-    export DECAY_STEPS={decay_step}
-    export DECAY_RATE={decay_rate}
-    export TASK={task}
+export TRAIN_STEPS=10000
+export MIN_EVAL=1000
+export BATCH_SIZE=50
+export LEARNING_RATE={learning_rate}
+export SAVE_CHECKPOINTS_STEPS=$TRAIN_STEPS
+export KEEP_RATE={keep_rate}
+export EMBED_SIZE={embed_sizes}
+export IN_LAYERS={num_layers}
+export IN_HIDDEN={hidden}
+export IN_KERNEL_SIZE={hidden}
+export OUT_LAYERS=2
+export OUT_HIDDEN=16,16
+export OUT_KERNEL_SIZE=4,4
+export MAX_LENGHT=1200 #1891
+export DECAY_STEPS={decay_step}
+export DECAY_RATE={decay_rate}
+export TASK={task}
     """.format(**search)
 
     run = """
-    export VOCAB_SOURCE=${DATA_PATH}/vocab.txt
-    export VOCAB_TARGET=${DATA_PATH}/vocab.txt
-    export TRAIN_SOURCES=${DATA_PATH}/seq2seq/questions_context_${TASK}_train
-    export TRAIN_TARGETS=${DATA_PATH}/seq2seq/answers_${TASK}_train
-    export DEV_SOURCES=${DATA_PATH}/seq2seq/questions_context_${TASK}_val
-    export DEV_TARGETS=${DATA_PATH}/seq2seq/answers_${TASK}_val
-    export TEST_SOURCES=${DATA_PATH}/seq2seq/questions_context_${TASK}_test
-    export TEST_TARGETS=${DATA_PATH}/seq2seq/answers_${TASK}_test
+export VOCAB_SOURCE=${DATA_PATH}/vocab.txt
+export VOCAB_TARGET=${DATA_PATH}/vocab.txt
+export TRAIN_SOURCES=${DATA_PATH}/seq2seq/questions_context_${TASK}_train
+export TRAIN_TARGETS=${DATA_PATH}/seq2seq/answers_${TASK}_train
+export DEV_SOURCES=${DATA_PATH}/seq2seq/questions_context_${TASK}_val
+export DEV_TARGETS=${DATA_PATH}/seq2seq/answers_${TASK}_val
+export TEST_SOURCES=${DATA_PATH}/seq2seq/questions_context_${TASK}_test
+export TEST_TARGETS=${DATA_PATH}/seq2seq/answers_${TASK}_test
 
-    sh conv_task.sh
+sh conv_task.sh
     """
     job_content = header + content + run
 
